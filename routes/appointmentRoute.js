46b7006b111d34d7ea90705e7ebd688726c5e8dc
Hoang Vu <vhoang2399@gmail.com>
2024-01-18 22:40:36 +0200

    map-token

--- routes/appointmentRoute.js
+++ routes/appointmentRoute.js
@@ -57,5 +57,48 @@ router.get('/:userId', async(req, res)=> {
   }
 })
 
+// get all appointment for a client
+router.get('/client/:userId', async(req, res)=> {
+
+  const {userId} = req.params;
+  // console.log(userId)
+
+  try {
+    // Use .exec() to get a promise from Appointment.find()
+    const appointments = await Appointment.find({ clientId: userId });
+
+    // Use map to find properties for each appointment
+    const bookedProperties = await Promise.all(appointments.map(async (appointment) => {
+      const property = await Property.findById(appointment.propertyId).exec();
+      return property;
+    }));
+
+    res.status(200).json({ appointments, bookedProperties });
+  } catch (e) {
+    res.status(400).json({ message: e.message });
+  }
+})
+
+// client delete an appointment
+router.delete('/:appointmentId', async (req, res)=> {
+  // destruct data
+  const {appointmentId} = req.params;  // id of appointment
+  const {userId} =  req.body; // id of user deleting
+
+  try {
+      // check if user has that appointment
+      const user = await User.findById(userId)
+      const deleteAppointment = await Appointment.findById(appointmentId)
+
+      // if(user._id !== deleteAppointment.clientId) return res.status(401).json("You don't have permission");
+      // delete
+      await Appointment.findByIdAndDelete(appointmentId);
+      const appointments = await Appointment.find();
+      res.status(201).json(deleteAppointment.clientId)
+  } catch(e) {
+      res.status(400).send(e.message);
+  }
+});
+
 
 module.exports = router;
